678cd5ce9b571408ef82cc692420111eda47be16
snowmossd-droid <snowmossd@gmail.com>
2026-05-23 06:01:27 +0700

    Update DiscordRPC.java

--- src/main/java/thunder/hack/utility/discord/DiscordRPC.java
+++ src/main/java/thunder/hack/utility/discord/DiscordRPC.java
@@ -1,7 +1,7 @@
-package vcore.utility.discord;
+package thunder.hack.utility.discord;
 
-import com.sun.jna.Library;
 import com.sun.jna.Native;
+import com.sun.jna.Library;
 
 public interface DiscordRPC extends Library {
     DiscordRPC INSTANCE;
@@ -9,28 +9,7 @@ public interface DiscordRPC extends Library {
     static {
         DiscordRPC instance = null;
         try {
-            String osName = System.getProperty("os.name").toLowerCase();
-            String osArch = System.getProperty("os.arch").toLowerCase();
-            
-            boolean isWindows = osName.contains("win");
-            boolean isMac = osName.contains("mac");
-            boolean isLinuxX64 = osName.contains("linux") && (osArch.contains("amd64") || osArch.contains("x86_64"));
-            boolean isLinuxArm = osName.contains("linux") && (osArch.contains("aarch64") || osArch.contains("arm"));
-            
-            String libName = "discord-rpc";
-            
-            if (isLinuxArm && !isWindows && !isMac) {
-                System.out.println("[DiscordRPC] Android/ARM64 detected - Discord RPC disabled");
-                instance = null;
-            }
-            else if (isWindows || isMac || isLinuxX64) {
-                System.out.println("[DiscordRPC] Loading on " + osName + " " + osArch);
-                instance = (DiscordRPC) Native.load(libName, DiscordRPC.class);
-            }
-            else {
-                System.out.println("[DiscordRPC] Unsupported platform: " + osName + " " + osArch);
-                instance = null;
-            }
+            instance = (DiscordRPC) Native.load("discord-rpc", DiscordRPC.class);
         } catch (Throwable e) {
             System.err.println("[DiscordRPC] Failed to load: " + e.getMessage());
             instance = null;
@@ -48,4 +27,4 @@ public interface DiscordRPC extends Library {
     void Discord_RunCallbacks();
     void Discord_Initialize(String var1, DiscordEventHandlers var2, boolean var3, String var4);
     void Discord_ClearPresence();
-}
+                            }
